3863fd860439b3fe7d6824997e06bf8e32dcb0d1
Aya Abdo <aya.yoya962015@gmail.com>
2025-07-24 13:10:57 +0200

    adding Repositories

---
+++ app/src/main/java/com/example/users/repository/UserRepository.kt
@@ -0,0 +1,20 @@
+package com.example.users.data.repository
+
+import com.example.users.data.User
+import com.example.users.data.UserDao
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class UserRepository @Inject constructor(
+    private val userDao: UserDao
+) {
+    suspend fun insertUser(user: User) {
+        userDao.insertUser(user)
+    }
+
+    fun getAllUsers(): Flow<List<User>> {
+        return userDao.getAllUsers()
+    }
+}
